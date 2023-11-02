6db6b5ed9facb3773d3fbadeb612acb107da3d99
Ort Rogozin <ort.rogozin@hotmail.com>
2023-11-02 09:38:16 +0200

    first commit

---
+++ Databases/Account_table.cs
@@ -0,0 +1,25 @@
+﻿using MySql.Data.MySqlClient;
+using Microsoft.Extensions.Configuration;
+using System.Configuration;
+
+namespace Databases
+{
+    public class Account_table : DB
+    {
+        List<string> acc_emails;
+        List<string> acc_usr_names;
+        List<string> acc_dobs;
+
+
+        public Account_table(List<string> acc_emails, List<string> acc_usr_names, List<string> acc_dobs)
+        {
+            this.acc_emails = acc_emails;
+            this.acc_usr_names = acc_usr_names;
+            this.acc_dobs = acc_dobs;
+
+            config.ConnectionString = base.settings.GetConnectionString("MySqlConnection");
+        }
+
+
+    }
+}
---
+++ Databases/DB.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using Microsoft.Extensions.Configuration;
+
+namespace Databases
+{
+    public class DB
+    {
+        public IConfigurationRoot settings = new ConfigurationBuilder()
+          .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+          .AddJsonFile("appsettings.json")
+          .Build();
+
+        public MySqlConnection config;
+    }
+    
+}
