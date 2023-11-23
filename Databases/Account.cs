05b483b64b82ecefb10972bfe1ebb5d0d1315491
Ort Rogozin <ort.rogozin@hotmail.com>
2023-11-23 10:39:13 +0200

    break data into classes

---
+++ Databases/Account.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Databases
+{
+
+    class Account
+    {
+        string accounts_email;
+        string accounts_user_name;
+        string accounts_date_of_birth;
+        string accounts_password;
+        
+        public bool AddIntoDB() 
+        {
+            
+        }
+    }
+}
--- Testing/Program.cs
+++ Testing/Program.cs
@@ -1,11 +1,16 @@
 ﻿using Database;
+using Databases.TableClasses;
+
 namespace Testing
 {
     class Program
     {
         public static void Main(string[] args)
         {
-            List<string> gmail_users = DBConnection.ExecuteQuery("SELECT * FROM table ").Result;
+            List<AccountTable> gmail_users = DBConnection.ExecuteQuery("SELECT accounts_email FROM my_project.accounts \r\n").Result;
+            Console.WriteLine(gmail_users.Count); ;
+            Console.Read();
         }
+
     }
 }
