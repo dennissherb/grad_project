73bffb23263acb02dfa0dfacd08cbd69d4c3c124
Ort Rogozin <ort.rogozin@hotmail.com>
2023-11-23 10:37:33 +0200

    TODO: fix json reader

--- Databases/DBConnection.cs
+++ Databases/DBConnection.cs
@@ -10,36 +10,35 @@ namespace Database
         private DBConnection()
         {
             //Create json reader
-            string fileName = "appsettings.json";
-            string jsonString = File.ReadAllText(fileName);
-            DBConnection connection = JsonSerializer.Deserialize<DBConnection>(jsonString)!;
+            //string filePath = Path.Combine(Directory.GetParent(System.IO.Directory.GetCurrentDirectory()).Parent.Parent.Parent.FullName, "Databases\\appsetting.json");
+            //string jsonString = File.ReadAllText(@"C:\Users\CSS\source\repos\grad_project\Databases\appsetting.json");
+            //DBConnection connection = JsonSerializer.Deserialize<DBConnection>(jsonString)!;
 
 
             //Get values from json file
-            this.Server = connection.Server;
-            this.DatabaseName = connection.DatabaseName;
-            this.User = connection.User;
-            this.Password = connection.Password;
+            //this.Server = connection.Server;
+            //this.DatabaseName = connection.DatabaseName;
+            //this.User = connection.User;
+            //this.Password = connection.Password;
 
             //Create the connection string
-            this.ConnectionString = String.Format("Server={0};Database={1};Uid={2};Pwd={3};", this.Server, this.DatabaseName, this.User, this.Password);
+            //this.ConnectionString = String.Format("Server={0};Database={1};Uid={2};Pwd={3};", this.Server, this.DatabaseName, this.User, this.Password);
+            this.ConnectionString = "Server=localhost;Database=my_project;Uid=root;Pwd=josh17rog";
         }
 
-        public string Server { get { return Server; }  set { Server = value; } }
-        public string DatabaseName { get { return DatabaseName; } set { DatabaseName = value; } }
-        public string User { get { return User; } set { User = value; } }
-        public string Password { get { return Password; } set { Password = value; } }
-        public string ConnectionString { get { return ConnectionString; } set { ConnectionString = value; } }
+        //public string Server { get { return Server; }  set { Server = value; } }
+        //public string DatabaseName { get { return DatabaseName; } set { DatabaseName = value; } }
+        //public string User { get { return User; } set { User = value; } }
+        //public string Password { get { return Password; } set { Password = value; } }
+        public string ConnectionString { get; set; }
 
 
-        public static async Task<List<Type>> ExecuteQuery(string query)
+        public static async Task<List<Object>> ExecuteQuery(string query)
         {
             DBConnection connectionObj = new DBConnection();
-            List<Type> result = new List<Type>();
+            List<Object> result = new List<Object>();
             using var connection = new MySqlConnection(connectionObj.ConnectionString);
-
             await connection.OpenAsync();
-
             using var command = new MySqlCommand(query, connection);
             using var reader = await command.ExecuteReaderAsync();
             while (await reader.ReadAsync())
@@ -47,6 +46,7 @@ namespace Database
                 var value = reader.GetValue(0);
                 Console.WriteLine(reader.GetValue(0));
             }
+            await connection.CloseAsync();
             return result;
         }
     }
