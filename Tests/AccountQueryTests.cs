0b3ef58b6d97b2cd7d38bad44ec72be82ee73090
Debian Dennis <qkweint@gmail.com>
2023-11-30 22:05:01 +0200

    clean

--- Tests/AccountQueryTests.cs
+++ Tests/AccountQueryTests.cs
@@ -8,9 +8,6 @@ namespace AccountQueryTests
     [TestClass]
     public class AccountQueryTests
     {
-   
-        
-
         [TestMethod]
         public async Task Test1TryLogin_InvalidCredentials()
         {
