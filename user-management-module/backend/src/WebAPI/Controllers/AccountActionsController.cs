7ec5085cb67c0146794147c10ba5b3d5543af3ed
Ort Rogozin <ort.rogozin@hotmail.com>
2023-12-06 09:39:58 +0200

    Added API project

---
+++ user-management-module/backend/src/WebAPI/Controllers/AccountActionsController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Datalayer.Queries;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountActionsController : ControllerBase
+    {
+        private readonly ILogger<AccountActionsController> _logger;
+        public AccountActionsController(ILogger<AccountActionsController> logger)
+        {
+            this._logger = logger;
+        }
+
+        [HttpGet("{email:long}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public ActionResult<AccountActionsController> Get(long uid, string pass)
+        {
+            if (uid <= 0) { return BadRequest(); }
+            Dictionary<string, string> account = AccountQuery.ReadAccountByEmail(email) ;
+            if (bu == null) { return NotFound(); }
+            return Ok(bu);
+        }
+
+
+
+    }
+}
