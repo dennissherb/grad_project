cb7f700567a39b72eb8af08571d13211002e04c6
Debian Dennis <qkweint@gmail.com>
2023-12-09 14:25:14 +0200

    moved folder

--- user-management-module/backend/WebAPI/Controllers/AccountController.cs
+++ user-management-module/backend/WebAPI/Controllers/AccountController.cs
@@ -17,12 +17,23 @@ namespace WebAPI.Controllers
 
         [HttpGet("GetByEmail/{email}")]
         [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public async Task<ActionResult<Dictionary<string, string>>> GetAsync(string email)
         {
             if (email == null) { return BadRequest(); }
             Dictionary<string, string> account = await AccountQuery.ReadAccountByEmail(email);
             if (!account.ContainsKey("accounts_id")) { return NotFound(); }
             return Ok(account);
+        }
+
+        [HttpGet("GetEntireTable")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<Dictionary<string, string>>> GetEntireTable()
+        {
+            List<Dictionary<string, string>> table = await AccountQuery.ReadAccounts();
+            if(table.Count < 0) return NotFound();
+            return Ok(table);
         }
     }
 }
