7d9fd43079be5370e98275667860e4f90eefb880
dennis <qkweint@gmail.com>
2024-03-02 21:00:16 +0200

    WIP:document and implement

--- user-management-module/frontend/blazor/Program.cs
+++ user-management-module/frontend/blazor/Program.cs
@@ -2,6 +2,8 @@ using blazor.Components;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddBlazorBootstrap();
+
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
