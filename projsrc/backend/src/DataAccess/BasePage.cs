d29f20b5c09d05b01ec5f2d16b435ec89cb9edaf
dennis <qkweint@gmail.com>
2024-04-03 20:56:51 +0300

    prepared classes

---
+++ projsrc/backend/src/DataAccess/BasePage.cs
@@ -0,0 +1,15 @@
+﻿namespace Datalayer;
+
+public class BasePage
+{
+    public string Title;
+    public string Author;
+    public string Content;
+
+    public BasePage(string title, string content, string author = "Anonymous")
+    {
+        Title = title;
+        Author = author;
+        Content = content;
+    }
+}
---
+++ projsrc/backend/src/DataAccess/Product.cs
@@ -0,0 +1,32 @@
+﻿namespace Datalayer
+{
+    public enum ProductCategory
+    {
+        Food,
+        Electronics,
+        Clothing,
+        Beauty,
+        Home,
+        Other
+    }
+
+    public class Product
+    {
+        public double Price { get; set; }
+        public string Name { get; set; }
+        public string Company { get; set; }
+        public byte[] Picture { get; set; }
+        public ProductCategory Category { get; set; }
+        public float Grading { get; set; }
+
+        public Product(double price, string name, string company, byte[] picture, ProductCategory category, float grading)
+        {
+            Price = price;
+            Name = name;
+            Company = company;
+            Picture = picture;
+            Category = category;
+            Grading = grading;
+        }
+    }
+}
