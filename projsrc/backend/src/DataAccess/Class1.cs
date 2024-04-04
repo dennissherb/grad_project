5f0f540f1958016deea57df727cb42c12d4e0774
dennis <qkweint@gmail.com>
2024-04-04 09:32:32 +0300

    added Content class

--- projsrc/backend/src/DataAccess/Class1.cs
+++ projsrc/backend/src/DataAccess/Class1.cs
@@ -4,11 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-
 namespace Datalayer
 {
     public class Content
     {
+        int ContentId;
+        int path;
         
     }
 }
